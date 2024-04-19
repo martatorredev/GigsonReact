2e9e4349190218a4a6faba9036a2841ed3c48c64
Marta Torre <hola@martatorre.dev>
2024-04-19 12:40:55 +0200

    Ready for review

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -15,10 +15,13 @@ function Hero() {
       </div>
       </header>
       <footer className="hero-godown">
-        <a className='hero-godown-a'>GO DOWN TO EXPLORE</a>
-        <svg className='icon-godown' viewBox="0 0 19 8" xmlns="http://www.w3.org/2000/svg">
+        <div className='hero-vtn-mas-gd'>
+          <a className='hero-godown-a'>GO DOWN TO EXPLORE</a>
+            <svg className='icon-godown' viewBox="0 0 19 8" xmlns="http://www.w3.org/2000/svg">
           <path d="M1 0.999999L8.92332 6.59293C9.26904 6.83697 9.73096 6.83697 10.0767 6.59293L18 1" stroke='#7874f4'/>
         </svg>
+        </div>
+        
       </footer>
     </section>
   )
