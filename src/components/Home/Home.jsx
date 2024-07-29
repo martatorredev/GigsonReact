661eb9c2826363bb283fcdc61b18988871808beb
renzi-fidele-frontend <renzifidele2001@gmail.com>
2024-07-30 00:14:52 +0200

    Corrección de la alineación del botón de reinicio del filtro de acordeón

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,13 +5,10 @@ import Form from "../Form";
 import HomeCases from "./HomeCases";
 import { CtaServices } from "./CtaServices";
 
-//  TODO: Corrigir a largura máxima do app que está exagerada
 //  TODO: Traduzir: CTO as a Service, Consultoria Tecnologica, Desarrollo Software, CTO as a service
 //  TODO: Corrigir número em liláz
 //  TODO: Aumentar o tamanho dos ícones, verificar em todos os lugares
-//  TODO: Separar mais as formas
 //  TODO: Corrigir o tamanho da fonte para que fique igual ao figma
-//  TODO: Fazer um scroll smooth ao topo ao se mudar de página
 
 const Home = () => {
    return (
